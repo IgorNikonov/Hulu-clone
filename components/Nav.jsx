943b5847eef0409b6115c517932a7b79e5898a9e
Igor Nikonov <igor.nikonov.07@gmail.com>
2022-06-29 13:19:45 +0200

    initial commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -2,35 +2,23 @@ import React from 'react'
 import requests from '../utils/requests'
 import { useRouter } from 'next/router'
 
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-
 const Nav = () => {
 	const router = useRouter();
 
 	return (
 		<nav className='relative'>
-			<div className='relative flex px-10 sm:px-20 text-2xl whitespace-nowrap sm:space-x-20 overflow-x-scroll scrollbar-hide'>
-				<Swiper
-					// slidesPerView={1}
-					breakpoints={{
-						320: {slidesPerView: 2.3, spaceBetween: 10},
-						// 320: {slidesPerView: 2.2},
-					}}
-				>
-					{Object.entries(requests).map(([key, {title, url}]) => (
-						<SwiperSlide key={key}>
-							<h2 
-								onClick={() => router.push(`/?genre=${key}`)}
-								className='cursor-pointer duration-100 hover:scale-125 hover:text-white active:text-red-500 last:pr-24'
-							>
-								{title}
-							</h2>
-						</SwiperSlide>
-					))}
-				</Swiper>
+			<div className=' flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide'>
+				{Object.entries(requests).map(([key, {title, url}]) => (
+					<h2 
+						key={key} 
+						onClick={() => router.push(`/?genre=${key}`)}
+						className='cursor-pointer duration-100 hover:scale-125 hover:text-white active:text-red-500 last:pr-24'
+					>
+						{title}
+					</h2>
+				))}
 			</div>
-			<div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12 z-50' />
+			<div className='absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12' />
 		</nav>
 	)
 }
